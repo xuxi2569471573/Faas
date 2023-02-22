4afefe2189b6b96418c0f8af7dda77491514324e
xuxi2569471573 <71211938+xuxi2569471573@users.noreply.github.com>
2023-02-22 14:48:36 +0800

    Create avatar.js

---
+++ netlify/functions/avatar.js
@@ -0,0 +1,41 @@
+import { identicon } from 'minidenticons';
+
+const WHITE_LIST = process.env.WHITE_LIST || '';
+const MAX_AGE = process.env.MAX_AGE || '3600';
+
+exports.handler = async function (event, context) {
+    let
+        whiteList = WHITE_LIST,
+        referer = event.headers.referer || '';
+    if (whiteList && referer) {
+        let
+            allowed = false,
+            domains = whiteList.split(',');
+        if (referer.startsWith('https://')) {
+            referer = referer.substring(8);
+        } else if (referer.startsWith('http://')) {
+            referer = referer.substring(7);
+        }
+        for (let domain of domains) {
+            if (referer.startsWith(domain + '/')) {
+                allowed = true;
+                break;
+            }
+        }
+        if (!allowed) {
+            return {
+                statusCode: 403,
+                body: 'Forbidden'
+            };
+        }
+    }
+    let name = (event.queryStringParameters.name || 'unnamed').trim();
+    return {
+        statusCode: 200,
+        headers: {
+            'cache-control': 'public, max-age=' + MAX_AGE,
+            'content-type': 'image/svg+xml'
+        },
+        body: identicon(name)
+    };
+};
